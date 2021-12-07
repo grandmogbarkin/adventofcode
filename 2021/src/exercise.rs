4781cf34a707a57f7d8ca2420f589e1dc98a921f
Constantinos Neophytou <1714925+grandmogbarkin@users.noreply.github.com>
2021-12-07 19:55:25 +0000

    Exercise template cleanup
    Remove unnecessary structs
    Clean up filename formatting code
    Add default binary to run

--- 2021/src/exercise.rs
+++ 2021/src/exercise.rs
@@ -2,23 +2,6 @@ extern crate simple_error;
 
 use std::error::Error as Error;
 
-pub struct Config {
-    pub task: String,
-    pub filename: String,
-}
-
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-      if args.len() < 2 {
-        return Err("not enough arguments");
-      }
-      let task = args[0].clone();
-      let filename = args[1].clone();
-
-      Ok(Config { task, filename })
-    }
-}
-
 pub trait SolutionT {
   fn day(&self) -> &str;
   fn test1_result(&self) -> i64;
@@ -31,11 +14,8 @@ pub struct Exercise { }
 
 impl Exercise {
   pub fn run(solution: &dyn SolutionT, test: bool) {
-    let filename = if test {
-      format!("inputs/input{}.test.txt", solution.day()).to_string()
-    } else {
-      format!("inputs/input{}.txt", solution.day()).to_string()
-    };
+    let test_str = if test { ".test" } else { "" };
+    let filename = format!("inputs/input{}{}.txt", solution.day(), test_str).to_string();
     let r1 = solution.task_1(filename.to_string());
     println!("Result for task 1: {:?}", r1);
     if test {
