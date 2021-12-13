3601cfeda8945318efbff21a18c92b1c5a2125d2
Constantinos Neophytou <1714925+grandmogbarkin@users.noreply.github.com>
2021-12-13 11:56:29 +0000

    Day 13
    Functional programming, code repetition, and magic numbers.
    
    Can't be bothered to fix with a migraine.

--- 2021/src/bin/day13.rs
+++ 2021/src/bin/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::error::Error;
 
 use advent2021::exercise::{Exercise, SolutionT};
@@ -11,24 +12,181 @@ impl SolutionT for Solution {
     }
 
     fn test1_result(&self) -> i64 {
-        1
+        17
     }
     fn test2_result(&self) -> i64 {
-        2
+        3072
     }
 
     fn task_1(&self, filename: String) -> Result<i64, Box<dyn Error>> {
-        let _test = read::read_lines(filename)?;
+        let input = read::read_lines(filename)?;
 
-        println!("Hello 1!");
-        Ok(1)
+        let mut board: Vec<Vec<bool>> = vec![];
+        let mut max_x: usize = 0;
+        let mut max_y: usize = 0;
+        for line in input.iter() {
+            if line == "" {
+                break;
+            }
+            let coords: Vec<usize> = line.split(",").map(|s| s.parse().unwrap()).collect();
+            if coords.len() != 2 {
+                simple_error::bail!(format!("Invalid coordinate input: {:?}", coords));
+            }
+            max_x = max(max_x, coords[0] + 1);
+            max_y = max(max_y, coords[1] + 1);
+
+            if max_y > board.len() {
+                board.resize(max_y, vec![false; max_x]);
+            }
+            if max_x > board[0].len() {
+                for col in board.iter_mut() {
+                    col.resize(max_x, false);
+                }
+            }
+            board[coords[1]][coords[0]] = true;
+        }
+        // println!("Board: {:?}", board);
+        let mut first_fold: String = "".to_string();
+        for line in input.iter().rev() {
+            if line == "" {
+                break;
+            }
+            first_fold = line.split(" along ").collect::<Vec<&str>>()[1].to_string();
+        }
+        let mut fold_split = first_fold.split("=");
+        let fold: (&str, usize) = (
+            fold_split.next().unwrap(),
+            fold_split.next().unwrap().parse().unwrap(),
+        );
+        println!("{:?}", fold);
+
+        match fold.0 {
+            "y" => {
+                let mut offset = (max_y - 1 - fold.1) * 2;
+                for y in (fold.1 + 1..max_y).rev() {
+                    for x in 0..max_x {
+                        board[y - offset][x] |= board[y][x];
+                        board[y][x] = false;
+                    }
+                    offset -= 2;
+                }
+            }
+            "x" => {
+                let mut offset = (max_x - 1 - fold.1) * 2;
+                for x in (fold.1 + 1..max_x).rev() {
+                    for y in 0..max_y {
+                        board[y][x - offset] |= board[y][x];
+                        board[y][x] = false;
+                    }
+                    offset -= 2;
+                }
+            }
+            _ => simple_error::bail!(format!("Invalid fold direction: {}", fold.0)),
+        }
+        // println!("Folded board: {:?}", board);
+        let mut dot_count: i64 = 0;
+        for i in board.iter().flatten() {
+            if *i {
+                dot_count += 1;
+            }
+        }
+        Ok(dot_count)
     }
 
     fn task_2(&self, filename: String) -> Result<i64, Box<dyn Error>> {
-        let _test = read::read_lines(filename)?;
+        let input = read::read_lines(filename)?;
 
-        println!("Hello 2!");
-        Ok(2)
+        let mut board: Vec<Vec<bool>> = vec![];
+        let mut max_x: usize = 0;
+        let mut max_y: usize = 0;
+        for line in input.iter() {
+            if line == "" {
+                break;
+            }
+            let coords: Vec<usize> = line.split(",").map(|s| s.parse().unwrap()).collect();
+            if coords.len() != 2 {
+                simple_error::bail!(format!("Invalid coordinate input: {:?}", coords));
+            }
+            max_x = max(max_x, coords[0] + 1);
+            max_y = max(max_y, coords[1] + 1);
+
+            if max_y > board.len() {
+                board.resize(max_y, vec![false; max_x]);
+            }
+            if max_x > board[0].len() {
+                for col in board.iter_mut() {
+                    col.resize(max_x, false);
+                }
+            }
+            board[coords[1]][coords[0]] = true;
+        }
+        let mut folds: Vec<(String, usize)> = vec![];
+        for line in input.iter().rev() {
+            if line == "" {
+                break;
+            }
+            let fold = line.split(" along ").collect::<Vec<&str>>()[1].to_string();
+            let mut fold_split = fold.split("=");
+            folds.push((
+                fold_split.next().unwrap().to_string(),
+                fold_split.next().unwrap().parse().unwrap(),
+            ))
+        }
+        folds.reverse();
+        println!("{:?}", folds);
+
+        for fold in folds {
+            match fold.0.as_str() {
+                "y" => {
+                    let mut offset = (max_y - 1 - fold.1) * 2;
+                    // println!("max_y: {}, y fold: {}, {}", max_y, fold.1, offset);
+                    for y in (fold.1 + 1..max_y).rev() {
+                        for x in 0..max_x {
+                            board[y - offset][x] |= board[y][x];
+                            board[y][x] = false;
+                        }
+                        offset -= 2;
+                    }
+                    max_y = fold.1;
+                    board.truncate(fold.1);
+                }
+                "x" => {
+                    let mut offset = (max_x - 1 - fold.1) * 2;
+                    // println!("max x: {}, x fold: {}, {}", max_x, fold.1, offset);
+                    for x in (fold.1 + 1..max_x).rev() {
+                        for y in 0..max_y {
+                            board[y][x - offset] |= board[y][x];
+                            board[y][x] = false;
+                        }
+                        offset -= 2;
+                    }
+                    for y in 0..max_y {
+                        board[y].truncate(fold.1);
+                    }
+                    max_x = fold.1;
+                }
+                _ => simple_error::bail!(format!("Invalid fold direction: {}", fold.0)),
+            }
+        }
+        for y in board.iter() {
+            for x in y {
+                if *x {
+                    eprint!("#")
+                } else {
+                    eprint!(".")
+                }
+            }
+            eprint!("\n");
+        }
+        let mut dot_count: i64 = 0;
+        let mut dot_sum: usize = 0;
+        for (i, v) in board.iter().flatten().enumerate() {
+            if *v {
+                dot_count += 1;
+                dot_sum += i;
+            }
+        }
+        Ok(dot_count * dot_sum as i64)
     }
 }
 
