f5fb21cab27fe4b2b8b490e8273a3c80e887a6fd
Constantinos Neophytou <1714925+grandmogbarkin@users.noreply.github.com>
2021-12-14 15:56:07 +0000

    Day 15 skeleton

---
+++ 2021/src/bin/day15.rs
@@ -0,0 +1,49 @@
+use std::error::Error;
+
+use advent2021::exercise::{Exercise, SolutionT};
+use advent2021::read;
+
+struct Solution {}
+
+impl SolutionT for Solution {
+    fn day(&self) -> &str {
+        "15"
+    }
+
+    fn test1_result(&self) -> i64 {
+        1
+    }
+    fn test2_result(&self) -> i64 {
+        2
+    }
+
+    fn task_1(&self, filename: String) -> Result<i64, Box<dyn Error>> {
+        let _test = read::read_lines(filename)?;
+
+        println!("Hello 1!");
+        Ok(1)
+    }
+
+    fn task_2(&self, filename: String) -> Result<i64, Box<dyn Error>> {
+        let _test = read::read_lines(filename)?;
+
+        println!("Hello 2!");
+        Ok(2)
+    }
+}
+
+pub fn main() {
+    let d = Solution {};
+    Exercise::run(&d, false)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_is_working() {
+        let d = Solution {};
+        Exercise::run(&d, true)
+    }
+}
