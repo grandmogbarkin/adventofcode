7eac67e92ceaeab13d14fabdab8681db83e11210
Constantinos Neophytou <1714925+grandmogbarkin@users.noreply.github.com>
2021-12-17 14:20:33 +0000

    Day 17
    Nice physics math for part 1. Brute force for part 2, with reasonable bounds.

--- 2021/src/bin/day17.rs
+++ 2021/src/bin/day17.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use advent2021::exercise::{Exercise, SolutionT};
 use advent2021::read;
+use regex::Regex;
 
 struct Solution {}
 
@@ -11,24 +12,72 @@ impl SolutionT for Solution {
     }
 
     fn test1_result(&self) -> i64 {
-        1
+        45
     }
     fn test2_result(&self) -> i64 {
-        2
+        112
     }
 
     fn task_1(&self, filename: String) -> Result<i64, Box<dyn Error>> {
-        let _test = read::read_lines(filename)?;
+        let input = read::read_lines(filename)?;
 
-        println!("Hello 1!");
-        Ok(1)
+        let re =
+            Regex::new(r"^target area: x=([-\d]+)\.\.([-\d]+), y=([-\d]+)..([-\d]+)$").unwrap();
+        let cap = re.captures(&input[0]).unwrap();
+
+        println!("Bounds: {:?}", cap);
+        let y_bottom = cap[3].parse::<i64>().unwrap();
+
+        // Vertical speed has no relation to horizontal position, and since horizontally the probe
+        //   will always stop moving at some point, only vertical speed matters.
+        // Vertically, any speed we shoot up with, will eventually be the speed the probe will be
+        //   coming down at. Maximum height means maximum downwards velocity, which will be the speed
+        //   at which point the probe at 0 is exactly 1 step away from the bottom of the target area,
+        //   which is a distance of y_bottom.
+        // So we need to shoot up at a velocity of n = -y_bottom - 1.
+        // Max height is n/2 * n+1
+        let n = -y_bottom - 1;
+        Ok(n * (n + 1) / 2)
     }
 
     fn task_2(&self, filename: String) -> Result<i64, Box<dyn Error>> {
-        let _test = read::read_lines(filename)?;
+        let input = read::read_lines(filename)?;
 
-        println!("Hello 2!");
-        Ok(2)
+        let re =
+            Regex::new(r"^target area: x=([-\d]+)\.\.([-\d]+), y=([-\d]+)..([-\d]+)$").unwrap();
+        let cap = re.captures(&input[0]).unwrap();
+
+        println!("Bounds: {:?}", cap);
+        let (x_left, x_right, y_bottom, y_top) = (
+            cap[1].parse::<i64>().unwrap(),
+            cap[2].parse::<i64>().unwrap(),
+            cap[3].parse::<i64>().unwrap(),
+            cap[4].parse::<i64>().unwrap(),
+        );
+        // We're assuming t_x>=0, t_y<=0 because rust ranges don't work backwards.
+        let mut count = 0;
+        // have to push to the right, max initial velocity is x_right.
+        for x_velocity in 0..x_right + 1 {
+            // Already established max upwards y velocity is -y_bottom - 1 in part 1.
+            // Also max downwards velocity is y_bottom.
+            for y_velocity in y_bottom..-y_bottom {
+                let mut x_v = x_velocity;
+                let mut y_v = y_velocity;
+                let (mut x, mut y) = (0, 0);
+                while x <= x_right && y >= y_bottom {
+                    if x >= x_left && y <= y_top {
+                        // println!("{},{}", x_velocity, y_velocity);
+                        count += 1;
+                        break;
+                    }
+                    x += x_v;
+                    y += y_v;
+                    y_v -= 1;
+                    x_v = std::cmp::max(0, x_v - 1);
+                }
+            }
+        }
+        Ok(count)
     }
 }
 
